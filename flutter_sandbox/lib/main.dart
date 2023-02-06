01c909acb364f169f2d30cd5c4039a14a8d7e1c4
Yann Gasner <yann.gasner@gmail.com>
2023-02-06 21:53:52 +0100

    posts

--- flutter_sandbox/lib/main.dart
+++ flutter_sandbox/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_sandbox/counter/counter_oberserver.dart';
-import 'package:flutter_sandbox/favorites/favorite.dart';
 
-import 'counter/counter.dart';
+import 'posts/posts.dart';
+import 'posts/simple_bloc_observer.dart';
 
 void main() {
-  Bloc.observer = CounterObserver();
-  runApp(const CounterApp());
+  Bloc.observer = SimpleBlocObserver();
+  runApp(const PostsApp());
 }
---
+++ flutter_sandbox/lib/posts/posts.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'view/posts_page.dart';
+
+class PostsApp extends MaterialApp {
+  const PostsApp({super.key}) : super(home: const PostsPage());
+}
